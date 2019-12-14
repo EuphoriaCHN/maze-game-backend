06711bb9535cfbad4384f871d3217bb622facefb
EuphoriaCHN <879969355@qq.com>
2019-12-14 20:45:56 +0800

    URL config

--- app/urls.py
+++ app/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 from django.conf.urls import url
-from django.views.generic import RedirectView
+from django.views.generic import RedirectView, TemplateView
 
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    url(r'^favicon.ico$', views.return_favicon_ico, name = 'favicon'), # 获得favicon.ico
+    path('', views.index, name = 'index'),
+    url(r'^favicon.ico$', views.return_favicon_ico, name = 'favicon'),  # 获得favicon.ico
 ]
